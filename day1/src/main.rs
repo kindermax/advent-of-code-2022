035d0577b0aba15d1048681c1a33038fed7d0346
Kindritskiy Max <kindritskiy.m@gmail.com>
2022-12-01 13:13:52 +0200

    day 1 - puzzle 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,6 +2,29 @@ use std::collections::HashMap;
 
 const INPUT: &str = include_str!("input.txt");
 
+fn puzzle1(elves_calories: &HashMap<usize, usize>) -> (usize, usize) {
+    let mut max_calories = 0;
+    let mut elf_id = 1;
+    for (id, calories) in elves_calories.iter() {
+        if calories > &max_calories {
+            max_calories = *calories;
+            elf_id = *id;
+        }
+    }
+
+    (elf_id, max_calories)
+}
+
+fn puzzle2(elves_calories: &HashMap<usize, usize>) -> usize {
+    // find sum of top 3 elves calories
+    let mut top_calories = elves_calories.values().cloned().collect::<Vec<usize>>();
+    top_calories.sort();
+    top_calories.reverse();
+    let top_calories_sum = top_calories[0] + top_calories[1] + top_calories[2];
+
+    top_calories_sum
+}
+
 fn main() {
     let mut elves_calories: HashMap<usize, usize>= HashMap::new();
     let mut elf_id = 1;
@@ -14,14 +37,9 @@ fn main() {
         elves_calories.entry(elf_id).and_modify(|e| *e += calories).or_insert(calories);
     }
 
-    let mut max_calories = 0;
-    let mut elf_id = 1;
-    for (id, calories) in elves_calories.iter() {
-        if calories > &max_calories {
-            max_calories = *calories;
-            elf_id = *id;
-        }
-    }
+    let (elf_id, max_calories) = puzzle1(&elves_calories);
     println!("Elf {} caries the most calories: {}", elf_id, max_calories);
+    let top_calories_sum = puzzle2(&elves_calories);
+    println!("Top 3 elves calories sum: {}", top_calories_sum);
 }
 
